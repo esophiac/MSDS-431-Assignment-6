ebc1b538903362dc5f535d1846283b0ef89ed6f9
Elizabeth Conwell <esophiac@gmail.com>
2025-05-05 21:17:35 -0400

    working on tests

--- image_processing/image_processing_test.go
+++ image_processing/image_processing_test.go
@@ -1,1 +1,23 @@
 package imageprocessing
+
+import (
+	//"fmt"
+	"testing"
+)
+
+// test the readImage function in image_processing to see if it returns an Image type
+func TestReadImage(t *testing.T) {
+	// test stuff here
+}
+
+func TestWriteImage(t *testing.T) {
+	// test stuff here
+}
+
+func TestGrayScale(t *testing.T) {
+	// test stuff here
+}
+
+func TestReSize(t *testing.T) {
+	// test stuff here
+}
